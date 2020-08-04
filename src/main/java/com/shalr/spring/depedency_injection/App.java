c322b702a2ab6762186b7fa24c4aa85baf43412d
tuanbt9874 <buithanhtuan9874@gmail.com>
2020-08-04 14:26:11 +0700

    Implement p:namespace that use when you have many property !

--- src/main/java/com/shalr/spring/depedency_injection/App.java
+++ src/main/java/com/shalr/spring/depedency_injection/App.java
@@ -2,11 +2,7 @@ package com.shalr.spring.depedency_injection;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.shalr.spring.depedency_injection.dao.ComputerDAO;
 import com.shalr.spring.depedency_injection.dao.ComputerDAOImpl;
-import com.shalr.spring.depedency_injection.dao.Dao;
-import com.shalr.spring.depedency_injection.dao.GennericComputerDAOImpl;
 import com.shalr.spring.depedency_injection.dto.Computer;
 
 /**
@@ -20,7 +16,7 @@ public class App
     	ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
     	try {
     		ComputerDAOImpl computerDAO = (ComputerDAOImpl) context.getBean("computerDAO");
-    		boolean result = computerDAO.insertComputer(new Computer("Apple-spring2022", "Apple", "Gray", 13, false));
+    		boolean result = computerDAO.insertComputer(new Computer("Apple-spring2025", "AP", "Gray", 13, false));
     		System.out.println(result ? "Insert successful !!!" : "Fail to insert !!!");
     	} catch (Exception e) {
 			e.printStackTrace();
